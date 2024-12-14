1e5974371547c77b8686797a1fa180666d5fc7d3
xuee <2176891692@qq.com>
2024-12-14 18:01:28 +0800

    Site updated: 2024-12-14 18:01:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/14/US.KG域名/","2024/12/14/hello-world/"];function toRandomPost(){
+var posts=["2024/12/14/hello-world/","2024/12/14/US.KG域名/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
